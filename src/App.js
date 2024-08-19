8ebd530a1ffbeadd67b94efce9a67f124cd338de
Mark <engineermarkpro@gmail.com>
2024-08-19 11:58:25 +0200

    add full screen map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
+import "leaflet/dist/leaflet.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="leaflet-container">
+      <MapContainer center={[53.10667, 6.87917]} zoom={12} style={{height: "100vh", width: "100vw"}}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {/* Additional map layers or components can be added here */}
+      </MapContainer>
+      </div>
+    </>
   );
 }
 
